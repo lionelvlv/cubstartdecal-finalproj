83be91592857a929ad3af919cc3a3bbe40481a5f
Lionel Verano <lionel.verano.iv@berkeley.edu>
2024-04-04 00:53:48 -0700

    fixed scripts file and added stuff to response box

--- scripts-folder/main.js
+++ scripts-folder/main.js
@@ -2,3 +2,11 @@
 const responseboxElem = document.getElementById('response-box');
 
 const inputElem = document.getElementById('input');
+
+function appendResponse(response) {
+    responseboxElem.innerHTML += response + '<br>';
+}
+
+appendResponse('Hello, World!');
+appendResponse('line 2');
+appendResponse('line 3!!!!!1');
