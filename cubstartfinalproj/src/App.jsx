1375cebcc1e93962f43b5af9be195904821f0eb2
Jerry Chen <123593638+kir-ran@users.noreply.github.com>
2024-04-09 02:55:46 -0700

    Revert "Backendmagic"

--- cubstartfinalproj/src/App.jsx
+++ cubstartfinalproj/src/App.jsx
@@ -1,92 +1,34 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [value, setValue] = useState(null)
-  const [message, setMessage] = useState(null)
-  const [previousChats, setPreviousChats] = useState([])
-  const [currentTitle, setCurrentTitle] = useState(null)
-
-  const handleClick = (uniqueTitle) => {
-    setCurrentTitle(uniqueTitle)
-    setValue("")
-    setMessage(null)
-  }
-  const createNewChat = () => {
-    setCurrentTitle(null)
-    setValue("")
-    setMessage(null)
-  }
-
-  const getMessages = async () => {
-    const options = {
-      method: "POST",
-      body: JSON.stringify({
-        message: value
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-    try {
-      const response = await fetch('http://localhost:8000/completions', options)
-      const data = await response.json()
-      console.log(data)
-      setMessage(data.choices[0].message)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    console.log(currentTitle, value, message)
-if (!currentTitle && value && message) {
-  setCurrentTitle(value)
-} 
-else if (currentTitle && value && message) {
-  setPreviousChats(prevChats => (
-    [...prevChats, 
-      {
-        title: currentTitle,
-        role: 'user',
-        content: value
-      },
-      {
-        title: currentTitle,
-        role: message.role,
-        content: message.content
-      }
-    ]
-  ))
-  }
-}, [message, currentTitle])
-  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
-  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
-  console.log(uniqueTitles)
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <section className="sidebar">
-        <button onClick={createNewChat}>+ New Chat</button>
-        <ul className="history">
-          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
-        </ul>
-      </section>
-      <section className="main">
-        {!currentTitle && <h1>Office Hours AI Tutor</h1>}
-        <ul className="feed">
-          {currentChat.map((chatMessage, index) => <li key={index}>
-            <p className='role'>{chatMessage.role}</p>
-            <p className='message'>{chatMessage.content}</p>
-            </li>)}
-        </ul>
-      </section>
-      <div className='input-container'>
-        {/* Don't ask me what this does LOL */}
-        <input value={value} onChange={(e) => setValue(e.target.value)}/>
-        <div id="submit" onClick={getMessages}>click me</div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
