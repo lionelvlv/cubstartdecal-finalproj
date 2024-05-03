1e74cdec08608f3ed3e836955642ccc97221f670
Jrendon2449 <jrendon2449@berkeley.edu>
2024-05-03 01:15:07 -0700

    Update

--- cubstartfinalproj/src/pages/AboutUs.jsx
+++ cubstartfinalproj/src/pages/AboutUs.jsx
@@ -6,15 +6,19 @@ function AboutUs() {
     <div>
       <Navbar />
       {/* Add stuff about our project here. This is the about us page. */}
-      <h1>Welcome to Office Hours!</h1>
-      <p>Cubstart Final Project Spring 2024</p>
-      <p>Made by Lionel, Jerry, Jose and Jordan</p>
-      <p>Frontend: Jose and Jordan</p>
-      <p>Backend: Lionel and Jerry</p>
-      <p></p>
-      <h1>Problem Statement</h1>
-      <p>Need additional help on homework assignments?</p>
-      <p>Look no further than our website Office Hours.</p>
+      <div className="info-box">
+        <h1>Welcome to Office Hours!</h1>
+        <p>Cubstart Final Project Spring 2024</p>
+        <p>Made by Lionel, Jerry, Jose and Jordan</p>
+        <p>Frontend: Jose and Jordan</p>
+        <p>Backend: Lionel and Jerry</p>
+        <p></p>
+        </div>
+        <div className="info-box">
+          <h1>Problem Statement</h1>
+          <p>Need additional help on homework assignments?</p>
+          <p>Look no further than our website Office Hours.</p>
+          </div>  
     </div>
   );
 }
